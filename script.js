805cfa29f801a1d6c0a39d196ee23059908a68a2
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-12 10:55:14 +0500

    insert parse

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const debounce = require('lodash.debounce');
-const chokidar = require('chokidar');
-const program = require('caporal');
-const fs = require('fs');
-const { spawn } = require('child_process');
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+const program = require("caporal");
+const fs = require("fs");
+const { spawn } = require("child_process");
 program
-  .version('0.0.1')
-  .argument('[filename]', 'Name of a file to execute')
+  .version("0.0.1")
+  .argument("[filename]", "Name of a file to execute")
   .action(async ({ filename }) => {
-    const name = filename || 'index.js';
+    const name = filename || "index.js";
 
     try {
       await fs.promises.access(name);
@@ -16,12 +16,14 @@ program
     }
 
     const start = debounce(() => {
-      spawn('node', [name], { stdio: 'inherit' });
+      spawn("node", [name], { stdio: "inherit" });
     }, 100);
 
     chokidar
-      .watch('.')
-      .on('add', start)
-      .on('change', start)
-      .on('unlink', start);
-  })
+      .watch(".")
+      .on("add", start)
+      .on("change", start)
+      .on("unlink", start);
+  });
+
+program.parse(process.argv);
