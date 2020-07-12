51bddf27cd63f1eda664ae192e77ca8e6b758d89
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-12 10:42:28 +0500

    insert require statements

---
+++ script.js
@@ -0,0 +1,5 @@
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
