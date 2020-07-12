fa9296149db10db72669f3a21de50f5df73d7d18
faiza203 <60883181+faiza203@users.noreply.github.com>
2020-07-12 10:53:46 +0500

    insert program

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const chokidar = require('chokidar');
 const program = require('caporal');
 const fs = require('fs');
 const { spawn } = require('child_process');
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
+
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      spawn('node', [name], { stdio: 'inherit' });
+    }, 100);
+
+  })
